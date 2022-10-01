3eec72bdcd4670a4d0c85fc1f7276420d0bf45aa
Johannes Hörmann <j.hoermann@tiffinger-thiel.de>
2022-10-01 21:02:08 +0200

    Add switches

--- scenes/map.gd
+++ scenes/map.gd
@@ -1,5 +1,9 @@
 extends TileMap
 
+@export_range(0, 100) var layer_track: int = 1
+
+const DATA_IS_TRACK = "IsSwitch"
+const DATA_SWITCH_TO = "SwitchTo"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,15 +11,29 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+	
+func get_tile_data(coordinate: Vector2i): 
+	if get_cell_source_id(layer_track, local_to_map(coordinate)) == -1:
+		return
+	return get_cell_tile_data(layer_track, local_to_map(coordinate))
+
+func switch(coordinate: Vector2): 
+	var tile = get_tile_data(coordinate)
+	if tile == null:
+		return
+		
+	if !tile.get_custom_data(DATA_IS_TRACK):
+		return
+	
+	var switch_to: Vector2i = tile.get_custom_data(DATA_SWITCH_TO)
+	
+	var source_id = get_cell_source_id(layer_track, local_to_map(coordinate))
+	set_cell(layer_track, local_to_map(coordinate), source_id, switch_to)
 
 func _input(event):
-   # Mouse in viewport coordinates.
-   if event is InputEventMouseButton:
-	   print("Mouse Click/Unclick at: ", event.position)
-   elif event is InputEventMouseMotion:
-	   print("Mouse Motion at: ", event.position)
-
-   # Print the size of the viewport.
-   print("Viewport Resolution is: ", get_viewport_rect().size)
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:	
+		if event.pressed:
+			switch(event.position)
