e7a7ead76cb2bf144006690000cc4b4b5dca5c1a
Johannes Hörmann <j.hoermann@tiffinger-thiel.de>
2022-10-01 13:28:07 +0200

    Init

---
+++ Trainz.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="Trainz" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="curve.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="straight.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="none.png"/>
+ </tile>
+</tileset>
