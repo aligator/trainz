3a4c6e6fd8af6f749c47e45dfca2e44766de08b7
Johannes Hörmann <j.hoermann@tiffinger-thiel.de>
2022-10-02 09:29:15 +0200

    Add depots

--- map/tiles/Trainz.tsx
+++ map/tiles/Trainz.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="Trainz" tilewidth="16" tileheight="16" tilecount="5" columns="0">
+<tileset version="1.8" tiledversion="1.8.6" name="Trainz" tilewidth="16" tileheight="16" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="curve.png"/>
@@ -15,5 +15,17 @@
  </tile>
  <tile id="4">
   <image width="16" height="16" source="switch2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="depot_orange.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="depot_blue.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="depot_yellow.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="depot_red.png"/>
  </tile>
 </tileset>
